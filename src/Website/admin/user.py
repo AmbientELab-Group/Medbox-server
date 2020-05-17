07dc51ca67dfcbd2bb11ce1a9741555ace6c4bf9
Krzysztof Adamkiewicz <kadamkiewicz835@gmail.com>
2020-05-17 14:35:02 +0200

    Move user forms from a separate package to user admin file

--- src/Website/admin/user.py
+++ src/Website/admin/user.py
@@ -1,5 +1,5 @@
 """
-Admin page for the user.
+Code that makes edyting users throught admin page possible.
 """
 
 __author__ = "Krzysztof Adamkiewicz"
@@ -8,10 +8,25 @@ __date__ = "11.5.2020"
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-from ..forms import UserCreationForm, UserChangeForm
 from ..models import User
 
+class UserCreationForm(UserCreationForm):
+    """
+    Form for creating a new user.
+    """
+    class Meta(UserCreationForm):
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+class UserChangeForm(UserChangeForm):
+    """
+    Form for edyting an existing user.
+    """
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
 
 class UserAdmin(UserAdmin):
     add_form = UserCreationForm
