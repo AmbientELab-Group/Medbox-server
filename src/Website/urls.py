bfca40ec2c586bd71afc32980c90e942a62b5e70
Krzysztof Adamkiewicz <kadamkiewicz835@gmail.com>
2020-05-25 17:36:14 +0200

    Make url config of the website more readible

--- src/Website/urls.py
+++ src/Website/urls.py
@@ -13,30 +13,72 @@ urlpatterns = [
     # for root redirect to login page
     path('', pages.index, name='index'),
     
-    # account management stuff
-    path('auth/login', pages.auth.login, name='login-page'), # login page
-    path('auth/logout', pages.auth.logout, name='logout-page'), # logout link, go here to logout
-    path('auth/register', pages.auth.register, name='registration-page'), # register a new user
+    # login page
+    path('auth/login', pages.auth.login, name='login-page'),
     
-    # account related pages
-    path('account/dashboard', pages.account.dashboard, name='user-dashboard'), # user dashboard page, you land there after login
-    path('account/edit', pages.account.edit, name='user-account-edit'), # user account edit page
-    path('account/passwd-change', pages.account.passwdChange, name='user-password-change'), # user account password change page
-    path('account/apps/dashboard', pages.account.apps.dashboard, name='user-apps-dashboard'), # paired applications dashboard
-    path('account/apps/add', pages.account.apps.add, name='user-apps-add'), # add new application dashboard
+    # logout link
+    path('auth/logout', pages.auth.logout, name='logout-page'),
     
-    # treatments
-    path('treatment/dashboard', pages.treatment.dashboard, name='treatment-dashboard'), # dashboard for the treatments
-    path('treatment/<str:treatmentID>/edit', pages.treatment.edit, name='treatment-edit'), # page for the treatment edits
-    path('treatment/<str:treatmentID>/exportToken', pages.treatment.exportToken, name='treatment-export-token'), # page for exporting tokens
-    path('treatment/<str:treatmentID>/medicine/<str:medicineID>/edit', pages.treatment.medicine.edit, name='treatment-medicine-edit'), # page for editing medicine
-    path('treatment/<str:treatmentID>/medicine/<str:medicineID>/delete', pages.treatment.medicine.delete, name='treatment-medicine-delete'), # page for deliting medicine
-    path('treatment/import', pages.treatment.importPage, name='treatment-import'), # page for the importing treatment
-    path('treatment/adherence', pages.treatment.adherence, name='treatment-adherence'), # page for adherence monitoring
+    # register a new user
+    path('auth/register', pages.auth.register, name='registration-page'),
     
-    # device management
-    path('devices/dispensor/dashboard', pages.devices.dispensor.dashboard, name='devices-dispensor-dashboard'), # dashboard page for all dispensors
-    path('devices/dispensor/add', pages.devices.dispensor.add, name='devices-dispensor-add'), # page for adding new dispensors
-    path('devices/dispensor/status', pages.devices.dispensor.status, name='devices-dispensor-status'), # page for displaying the status of new dispensors
-    path('devices/dispensor/refill', pages.devices.dispensor.refill, name='devices-dispensor-refill') # page for refilling the dispensor
+    # user dashboard page, you land there after login
+    path('account/dashboard', pages.account.dashboard, name='user-dashboard'),
+    
+    # user account edit page
+    path('account/edit', pages.account.edit, name='user-account-edit'),
+    
+    # user account password change page
+    path('account/passwd-change', pages.account.passwdChange,
+         name='user-password-change'),
+    
+    # paired applications dashboard
+    path('account/apps/dashboard', pages.account.apps.dashboard,
+         name='user-apps-dashboard'),
+    
+    # add new application dashboard
+    path('account/apps/add', pages.account.apps.add, name='user-apps-add'),
+    
+    # dashboard for the treatments
+    path('treatment/dashboard', pages.treatment.dashboard,
+         name='treatment-dashboard'),
+    
+    # page for the treatment edits
+    path('treatment/<str:treatmentID>/edit', pages.treatment.edit,
+         name='treatment-edit'),
+    
+    # page for exporting tokens
+    path('treatment/<str:treatmentID>/exportToken',
+         pages.treatment.exportToken, name='treatment-export-token'),
+    
+    # page for editing medicine
+    path('treatment/<str:treatmentID>/medicine/<str:medicineID>/edit',
+         pages.treatment.medicine.edit, name='treatment-medicine-edit'),
+    
+    # page for deliting medicine
+    path('treatment/<str:treatmentID>/medicine/<str:medicineID>/delete',
+         pages.treatment.medicine.delete, name='treatment-medicine-delete'),
+    
+    # page for the importing treatment
+    path('treatment/import', pages.treatment.importPage, name='treatment-import'),
+    
+    # page for adherence monitoring
+    path('treatment/adherence', pages.treatment.adherence,
+         name='treatment-adherence'),
+    
+    # dashboard page for all dispensors
+    path('devices/dispensor/dashboard', pages.devices.dispensor.dashboard,
+         name='devices-dispensor-dashboard'),
+    
+    # page for adding new dispensors
+    path('devices/dispensor/add', pages.devices.dispensor.add,
+         name='devices-dispensor-add'),
+    
+    # page for displaying the status of new dispensors
+    path('devices/dispensor/status', pages.devices.dispensor.status,
+         name='devices-dispensor-status'),
+    
+    # page for refilling the dispensor
+    path('devices/dispensor/refill', pages.devices.dispensor.refill,
+         name='devices-dispensor-refill')
 ]
