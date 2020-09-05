138691a8bd4a07e127b5c0150dbc6c308f34f75b
BalboBigguns <micholyea@gmail.com>
2020-09-05 19:12:11 +0200

    Added chamber initialization functionality in save method

--- backend/app/DeviceAPI/managers/__init__.py
+++ backend/app/DeviceAPI/managers/__init__.py
@@ -1,5 +1,3 @@
 __author__ = "Krzysztof Adamkiewicz"
 __status__ = "development"
 __date__ = "12.5.2020"
-
-from .user import UserManager
--- backend/app/DeviceAPI/models/container.py
+++ backend/app/DeviceAPI/models/container.py
@@ -1,47 +1,63 @@
 """
 Constraints:
-    - the position of the container has to be in range of device capacity based on device version
-    - on creation it checks there is a proper amount of chambers associated with this object
+    - the position of the container has to be in range of device capacity
+    based on device version
+    - on creation it checks there is a proper amount of chambers associated
+    with this object
 """
 
 __author__ = "Krzysztof Adamkiewicz"
 __status__ = "development"
-__date__ = "20.5.2020" 
+__date__ = "20.5.2020"
 
 from django.db import models
 import uuid as UUID
-from django.utils import timezone
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
-from django.db.models import Q
-from .chamber import Chamber
+from ..models import Chamber
 
 
 class Container(models.Model):
     # universal identifier
-    uuid = models.UUIDField(primary_key=True, default=UUID.uuid4, editable=False, unique=True)
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=UUID.uuid4,
+        editable=False,
+        unique=True
+    )
 
     # number of chambers in this container
     capacity = models.PositiveSmallIntegerField()
 
     # reference to device this container belongs to
-    device = models.ForeignKey("Device", on_delete=models.CASCADE, related_name="containers")
+    device = models.ForeignKey(
+        "Device",
+        on_delete=models.CASCADE,
+        related_name="containers"
+    )
 
     # position in the container
     position = models.PositiveSmallIntegerField()
 
     # time this container was refilled at
-    lastRefill = models.DateTimeField(null=True ,blank=True)
+    lastRefill = models.DateTimeField(null=True, blank=True)
 
     def clean(self):
         # validate if the position is in the correct range
         if self.position < 0 or self.position >= self.device.capacity:
-            raise ValidationError(_("Position value outside of container's capacity"), code="invalid_value")
+            raise ValidationError(
+                _("Position value outside of container's capacity"),
+                code="invalid_value"
+            )
 
-         # check if the chamber entities are associated with this container
-        numberOfConainers = Container.objects.filter(uuid=self.uuid).count()
-        if numberOfConainers != self.capacity:
-            raise ValidationError(_("Chambers not instantiated"), code="integrity_error")
+    def save(self, *args, **kwargs):
+        # create chamber entities when saving new object
+        if not Container.objects.filter(uuid=self.uuid).exists():
+            for pos in range(self.capacity):
+                chamber = Chamber(container=self, position=pos)
+                chamber.save()
+
+        super(Container, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"At position: {self.position}, last refilled: {self.lastRefill}"
