b4cca3c76a276a1065633272cfa2fd358934c000
Krzysztof Adamkiewicz <k.adamkiewicz@protonmail.com>
2020-11-12 18:29:21 +0100

    Remove random stuff from settings.
    This stuff is probably not necessary since django is no longer serving frontend.
    If something breakes reverse one or more changs related to installed apps and middleware.

--- backend/app/MedboxWeb/settings.py
+++ backend/app/MedboxWeb/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
     "AdminPanel.apps.AdminpanelConfig",
@@ -154,14 +153,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "Static")
-]
-
-# lifetime of the treatment token in hours
-TREATMENT_TOKEN_LIFETIME = 168
+STATIC_URL = ""
+STATICFILES_DIRS = []
 
 # lifetime of the pairing codes in minutes
 PAIRING_CODE_LIFETIME = 5
