d69dbb584d4ee5be77bd00ef246d33fc2e72eb61
BalboBigguns <micholyea@gmail.com>
2020-11-13 16:58:02 +0100

    Adminpanel css fix.

--- backend/app/MedboxWeb/settings.py
+++ backend/app/MedboxWeb/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "django.contrib.staticfiles",
     "corsheaders",
     "rest_framework",
     "AdminPanel.apps.AdminpanelConfig",
@@ -153,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = ""
+STATIC_URL = "/static/"
 STATICFILES_DIRS = []
 
 # lifetime of the pairing codes in minutes
