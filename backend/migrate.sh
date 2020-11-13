930663c75998a3784687be50680461340f0814d9
BalboBigguns <micholyea@gmail.com>
2020-11-13 16:55:06 +0100

    Added automatic superuser creation when executing resetDB script.

--- backend/migrate.sh
+++ backend/migrate.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # to run it inside a container type (in this directory):
 # docker exec -i medbox-backend_server bash < migrate.sh
-# don't forget to create superuser afterwards
 ./manage.py makemigrations AdminPanel
 ./manage.py makemigrations AppAPI
 ./manage.py makemigrations DeviceAPI
 ./manage.py migrate
+./manage.py createsuperuser --noinput
--- backend/resetDB.sh
+++ backend/resetDB.sh
@@ -1,7 +1,21 @@
-#!/bin/sh
-# removes all migration files and recreates "mainDB" database
+#!/bin/bash
+# removes all migration files, recreates "mainDB" database, performs all migrations and creates superuser
+read -p "This command will erease all your DB entries. Do you want to continue? (y/n) " answer
+case ${answer:0:1} in
+    y|Y ) ;;
+    * ) echo "Command aborted"; exit;;
+esac
 
+docker-compose stop medbox-database_server medbox-backend_server
 rm -r ./app/*/migrations/*
+
+docker-compose up -d medbox-database_server
 docker cp ./resetDB.sql medbox-database_server:/
 docker exec medbox-database_server /bin/sh -c 'psql -U postgres < /resetDB.sql'
 docker exec medbox-database_server /bin/sh -c 'rm resetDB.sql'
+
+docker-compose up -d medbox-backend_server
+docker exec -i medbox-backend_server bash < migrate.sh
+
+docker-compose stop medbox-database_server medbox-backend_server
+docker-compose up medbox-database_server medbox-backend_server
