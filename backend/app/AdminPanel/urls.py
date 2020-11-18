8b52b02c36b3bc47a53ee2a9761bee605e43073a
BalboBigguns <micholyea@gmail.com>
2020-11-18 23:40:56 +0100

    Urls cleanup according to the documentation. Fixed typo in container serializer.

--- backend/app/AdminPanel/urls.py
+++ backend/app/AdminPanel/urls.py
@@ -1,13 +1,16 @@
-__author__ = "Krzysztof Adamkiewicz"
-__status__ = "development"
-__date__ = "10.5.2020"
-
 from django.urls import path
 from AdminPanel.views import user as UserViews
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 urlpatterns = [
     path("users/<uuid:pk>", UserViews.user_info),
     path("signup", UserViews.sign_up_view),
-    path("signin", TokenObtainPairView.as_view())
+    path("signin", TokenObtainPairView.as_view()),
+    # JWT api
+    path("token/refresh", TokenRefreshView.as_view()),
+    path("token/verify", TokenVerifyView.as_view()),
 ]
