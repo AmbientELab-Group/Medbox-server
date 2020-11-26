34e798dca0546fd1f0141aca49f78b5eb368f678
Jakub Cytrowski <jakub.cytrowski@gmail.com>
2020-11-26 17:22:30 +0100

    Improve TelemetryLogView

--- backend/app/DeviceAPI/views/telemetryLog.py
+++ backend/app/DeviceAPI/views/telemetryLog.py
@@ -1,18 +1,16 @@
 from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from DeviceAPI.authentication import DeviceTokenAuthentication
 from DeviceAPI.serializers import TelemetrySerializer
 
 
 class TelemetryLogView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
+    serializer_class = TelemetrySerializer
 
     def create(self, request):
-        serializer = TelemetrySerializer(data=request.data)
-        token = DeviceTokenAuthentication.model.objects.get()
-        device_id = str(token.user).split(':')[1]
-        if serializer.is_valid():
-            serializer.save(device_id=device_id)
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(device_id=request.user.uuid)
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
