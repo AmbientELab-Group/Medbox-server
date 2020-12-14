c7cfe3f4347149939faf2c466fcfa6892ac9b2e2
Nexxtgaming <maciek.pilot2@gmail.comm>
2020-12-14 22:20:01 +0100

    added new class to test_device.py

--- backend/app/DeviceAPI/tests/views/test_device.py
+++ backend/app/DeviceAPI/tests/views/test_device.py
@@ -10,3 +10,33 @@ from DeviceAPI.models import (
 from uuid import UUID
 from collections import OrderedDict
 
+class DeviceTestCase(APITestCase):
+    device_pairing_url = "devices/pairing/confirm/"
+
+    def setUp(self):
+        self.owner = get_user_model().objects.create(
+            email="testemail@test.com",
+            password="testpassword"
+        )
+
+        tokens = RefreshToken.for_user(self.owner)
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + str(tokens.access_token))
+
+        self.device_version = DeviceVersion.objects.create(
+            name="0.0",
+            capacity=3,
+            latest_firmware_version="0.0.0"
+        )
+
+        self.device = Device.objects.create(
+            owner=self.owner,
+            version=self.device_version,
+            name="TestBox"
+        )
+
+    def test_pairing_confirm(self):
+        data = {
+            "device_uuid": self.device.uuid
+            "pairing_code": 
+        }
+        pass
